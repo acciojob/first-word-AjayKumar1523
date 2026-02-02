14976c9df815c1aa8d54b0420c5823c541dac8d9
AjayKumar1523 <143533253+AjayKumar1523@users.noreply.github.com>
2026-02-02 21:50:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function firstWord(s) {
-  // your code here
+  // If string is empty, return empty string
+  if (s === "") return "";
+
+  // Remove leading spaces
+  s = s.trimStart();
+
+  // Find first space
+  const index = s.indexOf(" ");
+
+  // If no space found, return entire string
+  if (index === -1) {
+    return s;
+	  
+  }
+
+  // Return substring up to first space
+  return s.substring(0, index);
 }
+
 
 // Do not change the code below
 
